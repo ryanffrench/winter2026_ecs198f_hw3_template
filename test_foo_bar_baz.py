544f32a209190e30118252dc1bfc1350c3fb9032
ffrench <ffrench2004@gmail.com>
2026-03-01 13:31:41 -0800

    Do assignment

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -8,6 +8,7 @@ def test_foo_bar_baz():
     assert foo_bar_baz(3) == "1 2 Foo"
     assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
     assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
+    assert foo_bar_baz(-1) == ""
 
 def test_error():
     with pytest.raises(TypeError):

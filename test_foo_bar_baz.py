df055eaaa268a989cf21da9a3fa9e1b87cb82fa1
ffrench <ffrench2004@gmail.com>
2026-03-01 13:09:24 -0800

    Do assignment

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -3,3 +3,8 @@ import pytest
 from foo_bar_baz import foo_bar_baz
 
 #Add testcases Here
+def test_foo_bar_baz():
+    assert foo_bar_baz(0) == ""
+    assert foo_bar_baz(3) == "1 2 Foo"
+    assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
+    assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"

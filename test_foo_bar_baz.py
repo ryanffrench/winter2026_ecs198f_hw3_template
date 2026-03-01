72e9a3e90be23f27d03dc38b6b7771e0cb88b022
ffrench <ffrench2004@gmail.com>
2026-03-01 13:21:34 -0800

    Do assignment

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -8,3 +8,13 @@ def test_foo_bar_baz():
     assert foo_bar_baz(3) == "1 2 Foo"
     assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
     assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
+
+def test_error():
+    with pytest.raises(TypeError):
+        foo_bar_baz("div ranch")
+    with pytest.raises(TypeError):
+        foo_bar_baz(67.67)
+
+def test_empty():
+    with pytest.raises(TypeError):
+        foo_bar_baz()
